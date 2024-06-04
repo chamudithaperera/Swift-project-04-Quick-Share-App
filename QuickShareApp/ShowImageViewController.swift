716e9812c18d414ac24d592d0ba92cef2f275ec9
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-06-04 23:07:22 +0530

    first commit

---
+++ QuickShareApp/ShowImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowImageViewController.swift
+//  QuickShareApp
+//
+//  Created by Chamuditha Kavishan on 2024-06-04.
+//
+
+import UIKit
+
+class ShowImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
